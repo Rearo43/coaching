1b15309d713003448c58cbdc31174aec7ac32b1d
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-03-19 12:52:56 -0700

    fixed js questions

--- add-content.js
+++ add-content.js
@@ -1,63 +1,46 @@
 //var answer = 'Game' 'game' 'Basketball' 
 var answers = prompt("What are you interested in, Basketball or Gaming?");
-var banana;
+var feedBack;
 
 if (answers === 'Gaming') {
-    confirm('cool, me too!')
-    banana = 'cool, me too!!';
+    confirm('You might want to try our Twitch Coaches link.')
+    feedBack = 'Gamers- Try out, https://masonchance.github.io/twitch-coaches/';
 } else if (answers === 'Basketball') {
     confirm('You are in the right place!')
-    banana = 'You are in the right place!';
+    feedBack = 'Ballers- You are in the right place!';
 } 
-//else if (answers === 'Basketball') {
- //   banana = '';
-//} else if (answers === 'basketball') {
-//   banana = '';
-//} else {
- //   banana = '';
-//}
 
-document.write('<h3>' + banana + '</h3>');
+
+document.write('<h3>' + feedBack + '</h3>');
 
 
 //////////////////////////////////////2
 var answers = prompt("Male or Female?");
-var banana;
+var feedBack;
 
 if (answers === 'Male') {
-    confirm('Great, we have coaches excited to talk to you')
-    banana = 'cool, me too!!';
+    confirm('Great, we have coaches excited to talk to you!')
+    feedBack = 'Male- Great, we have coaches excited to talk to you!';
 } else if (answers === 'Female') {
-    banana = 'Sounds great many of our female coaches are on their way to the WNBA!';
+    confirm('Sounds great many of our female coaches are on their way to the WNBA!')
+    feedBack = 'Female- Sounds great many of our female coaches are on their way to the WNBA!';
 } 
-//else if (answers === 'Basketball') {
- //   banana = '';
-//} else if (answers === 'basketball') {
-//   banana = '';
-//} else {
- //   banana = '';
-//}
 
-document.write('<h3>' + banana + '</h3>');
+
+document.write('<h3>' + feedBack + '</h3>');
 
 
 ///////////////////////////3
-var answers = prompt("Are you 18 or older?");
-var banana;
+var answers = prompt("Are you 18 or older? (Yes or No)");
+var feedBack;
 
 if (answers === 'Yes') {
-    confirm('Great continue')
-    banana = 'Great continue';
+    confirm('Great, continue!')
+    feedBack = 'You have confirmed you are over 18.';
 } else if (answers === 'No') {
-    confirm('Ask a parent to contact us.')
-    banana = 'Ask a parent to contact us.';
+    confirm('Ask a parent to contact us on your behalf.')
+    feedBack = 'Not over 18, just ask a parent to contact us on your behalf.';
 } 
-//else if (answers === 'Basketball') {
- //   banana = '';
-//} else if (answers === 'basketball') {
-//   banana = '';
-//} else {
- //   banana = '';
-//}
 
-document.write('<h3>' + banana + '</h3>');
+
+document.write('<h3>' + feedBack + '</h3>');
