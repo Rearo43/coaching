a6e9de2764abfad714ee3b6b9c05606d2016a2c4
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-03-18 21:19:42 -0700

    working

--- add-content.js
+++ add-content.js
@@ -1,17 +1,63 @@
-//var answer = 2;
+//var answer = 'Game' 'game' 'Basketball' 
 var answers = prompt("What are you interested in, Basketball or Gaming?");
-var innerHTML;
+var banana;
 
 if (answers === 'Gaming') {
-    innerHTML = 'https://masonchance.github.io/twitch-coaches/';
-} else if (answers === 'gaming') {
-    innerHTML = 'https://masonchance.github.io/twitch-coaches/';
+    confirm('cool, me too!')
+    banana = 'cool, me too!!';
 } else if (answers === 'Basketball') {
-    innerHTML = 'https://rearo43.github.io/coaching/index.html';
-} else if (answers === 'basketball') {
-    innerHTML = 'https://rearo43.github.io/coaching/index.html';
-} else {
-    innerHTML = 'https://rearo43.github.io/coaching/index.html';
-}
+    confirm('You are in the right place!')
+    banana = 'You are in the right place!';
+} 
+//else if (answers === 'Basketball') {
+ //   banana = '';
+//} else if (answers === 'basketball') {
+//   banana = '';
+//} else {
+ //   banana = '';
+//}
 
-document.write('<h3>' + innerHTML + '</h3>');
+document.write('<h3>' + banana + '</h3>');
+
+
+//////////////////////////////////////2
+var answers = prompt("Male or Female?");
+var banana;
+
+if (answers === 'Male') {
+    confirm('Great, we have coaches excited to talk to you')
+    banana = 'cool, me too!!';
+} else if (answers === 'Female') {
+    banana = 'Sounds great many of our female coaches are on their way to the WNBS!';
+} 
+//else if (answers === 'Basketball') {
+ //   banana = '';
+//} else if (answers === 'basketball') {
+//   banana = '';
+//} else {
+ //   banana = '';
+//}
+
+document.write('<h3>' + banana + '</h3>');
+
+
+///////////////////////////3
+var answers = prompt("Are you 18 or older?");
+var banana;
+
+if (answers === 'Yes') {
+    confirm('Great continue')
+    banana = 'Great continue';
+} else if (answers === 'No') {
+    confirm('Ask a parent to contact us.')
+    banana = 'Ask a parent to contact us.';
+} 
+//else if (answers === 'Basketball') {
+ //   banana = '';
+//} else if (answers === 'basketball') {
+//   banana = '';
+//} else {
+ //   banana = '';
+//}
+
+document.write('<h3>' + banana + '</h3>');
